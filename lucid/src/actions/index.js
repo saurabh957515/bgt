e3cfd12da2250023a5914b7bcae2a4d0b2a3b897
saurabh <rathvasaurabhkumar@gmail.com>
2024-08-18 12:14:12 +0530

    files deleted

--- lucid/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./LoginAction";
-export * from "./NavigationAction";
-export * from "./AnalyticalAction";
-export * from "./DemographicAction";
-export * from "./IoTAction";
-export * from "./MailInboxAction";
-export * from "./UIElementsAction";
